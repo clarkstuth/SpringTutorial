52747ff0338b1ea56695968eef3445c4a742acfb
Clark Stuth <clarkstuth@gmail.com>
2015-04-18 19:08:19 -0500

    starting annotation config

---
+++ GradleSpringProject2_AnnotationConfig/src/main/java/main/java/SpringTutorial/model/Customer.java
@@ -0,0 +1,26 @@
+package main.java.SpringTutorial.model;
+
+public class Customer {
+
+    private String firstname;
+    private String lastname;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    
+    
+}
